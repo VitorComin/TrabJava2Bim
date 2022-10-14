674c1493f686ab37ded918364902184530525045
VitorComin <106283734+VitorComin@users.noreply.github.com>
2022-10-14 20:08:16 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,28 +11,33 @@ public class Main {
     System.out.println ("Adicione suas tarefas:");
     System.out.println ("Tarefa 1:");
     String tarefa1 = ler.nextLine();
+    System.out.println ("Prioridade Tarefa 1(Alta,Media,Baixa..):");
+    String prioridade1 = ler.nextLine();
     System.out.println ("Tarefa 2:");
     String tarefa2 = ler.nextLine();
+    System.out.println ("Prioridade Tarefa 2(Alta,Media,Baixa..):");
+    String prioridade2 = ler.nextLine();
     System.out.println ("Tarefa 3:");
     String tarefa3 = ler.nextLine();
-
-    tarefas.add(tarefa1);
-    tarefas.add(tarefa2);
-    tarefas.add(tarefa3);
+    System.out.println ("Prioridade Tarefa 3(Alta,Media,Baixa..):");
+    String prioridade3 = ler.nextLine();
+        
+    tarefas.add(tarefa1, prioridade1);
+    tarefas.add(tarefa2, prioridade2);
+    tarefas.add(tarefa3, prioridade3);
 
     System.out.println(tarefas);
 
     System.out.println("Deseja concluir tarefa 1, 2 ou 3?:")
     Int concluir = nextInt();
     if (concluir == 1){
-        tarefas.remove(tarefa1);
+        tarefas.remove(tarefa1, prioridade1);
     } else {
         if (concluir == 2) {
-            tarefas.remove(tarefa2);
+            tarefas.remove(tarefa2, prioridade2);
         } else {
-            tarefas.remove(tarefa3);
+            tarefas.remove(tarefa3, prioridade3);
         }
     }
-    System.out.println("Que tarefa")
     }
 }
