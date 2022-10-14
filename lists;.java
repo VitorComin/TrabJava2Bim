267576ec443b06656ae0fa3ddc4b518c64d3e194
VitorComin <106283734+VitorComin@users.noreply.github.com>
2022-10-14 20:02:45 -0300

    Add files via upload

---
+++ lists;.java
@@ -0,0 +1,14 @@
+package trabalho;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public Tarefas {
+    ArrayList<String> tarefas = new ArrayList<>();
+
+}
+
+public Concluidas {
+    ArrayList<String> concluidas = new ArrayList();
+}
